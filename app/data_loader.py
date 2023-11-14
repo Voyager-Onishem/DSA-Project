df8395ae633ac6b67b30623f10a5f1d2c6873ad4
Sowmith Reddy <sowmith1810@gmail.com>
2023-11-14 21:23:44 +0530

    Bugfixes by Ronak pt.1

--- app/data_loader.py
+++ app/data_loader.py
@@ -2,22 +2,23 @@ import json
 import os
 metro_data=None
 def load_metro_network_data(data_file):
-    print(os.getcwd())
     global metro_data
+    #print(os.getcwd())
     try:
         with open(data_file, "r") as file:
             metro_data = json.load(file)
+            print(metro_data)
         return metro_data
     except FileNotFoundError:
         print(f"Error: File {data_file} not found.")
         return None
 
 if __name__ == "__main__":
-    data_file=os.getcwd()
-    data_file += "/Metro-Route-Planner/app/data/metro_data.json"
+    data_file = os.path.join(os.getcwd(), "app/data/metro_data.json")
+    print(data_file)
     metro_data = load_metro_network_data(data_file)
 
-    if metro_data:
+    #if metro_data:
         # Process the loaded metro network data
-        print("Metro Network Data Loaded Successfully:")
-        print(metro_data)
+        #print("Metro Network Data Loaded Successfully:")
+        #print(metro_data)
