b8d0064781a9eb3d1b466d197b39aafc33be4dfa
Sowmith Reddy <sowmith1810@gmail.com>
2023-11-14 21:22:03 +0530

    Bugfixes by Ronak 1.11

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify,Blueprint
+from flask import Flask,render_template, request, jsonify,Blueprint
 import route_planner
-from views import views 
+from views import views
 # Example graph
+'''
 metro_data = {
     "stations": [
         {"name": "Station A"},
@@ -13,14 +14,16 @@ metro_data = {
         {"station1": "Station B", "station2": "Station C", "distance": 3.0}
     ]
 }
-
+'''
+#creates the app and redirects where to look for
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,static_folder='static')
     app.register_blueprint(views,url_prefix="/")
-    
     return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+#run app here and go to http://localhost:5000/
